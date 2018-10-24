0f7bbf3c32679ee343d3fcb10d152f0776eb89e0
windows <windows@DESKTOP-GKAFK2R>
2018-10-24 07:53:10 +0530

    Commited by Vinod

---
+++ V-Mart/src/main/java/com/vedas/vmart/model/UserAddress.java
@@ -0,0 +1,85 @@
+package com.vedas.vmart.model;
+
+public class UserAddress {
+	
+	private String addressId;
+	private String Name;
+	private String Address;
+	private String Landmark;
+	private String pinCode;
+	private String Area;
+	private String City;
+	private String State;
+	private String Mobile;
+	private Boolean fevourite;
+	private String userId;
+	
+	public String getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(String addressId) {
+		this.addressId = addressId;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getLandmark() {
+		return Landmark;
+	}
+	public void setLandmark(String landmark) {
+		Landmark = landmark;
+	}
+	public String getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(String pinCode) {
+		this.pinCode = pinCode;
+	}
+	public String getArea() {
+		return Area;
+	}
+	public void setArea(String area) {
+		Area = area;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public String getMobile() {
+		return Mobile;
+	}
+	public void setMobile(String mobile) {
+		Mobile = mobile;
+	}
+	public Boolean getFevourite() {
+		return fevourite;
+	}
+	public void setFevourite(Boolean fevourite) {
+		this.fevourite = fevourite;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+}
---
+++ V-Mart/src/main/java/com/vedas/vmart/model/UserAddressList.java
@@ -0,0 +1,21 @@
+package com.vedas.vmart.model;
+
+public class UserAddressList {
+	
+	private String message;
+	private String response;
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getResponse() {
+		return response;
+	}
+	public void setResponse(String response) {
+		this.response = response;
+	}	
+
+}
